df37258e222947837cfd4e656e2e7ad0de7e19eb
Cyrille Guillaume <cyrilleguillaume1996@gmail.com>
2020-08-05 17:34:16 +0200

    Update index, scss, js

---
+++ src/assets/js/app.js
@@ -0,0 +1,17 @@
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
+
+const iconMenu = document.getElementById("ICON_MENU");
+const listNav = document.querySelector(".list-nav");
+const backgroundNav = document.querySelector(".list-nav-background");
+
+iconMenu.addEventListener("click", () => {
+    console.log("Hello");
+    backgroundNav.classList.toggle("show-nav");
+    listNav.classList.toggle("show-nav");
+
+    if (listNav.classList.contains("show-nav")) {
+        disableBodyScroll(listNav);
+    } else {
+        enableBodyScroll(listNav);
+    }
+});
