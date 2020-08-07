b47e527e94b256d9731db4e00b3f4d0cdb374b75
Cyrille Guillaume <cyrilleguillaume1996@gmail.com>
2020-08-07 15:48:04 +0200

    Test build mobile

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,18 +1,19 @@
 import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 
 const iconMenu = document.getElementById("ICON_MENU");
-const listNav = document.querySelector(".list-nav");
-const backgroundNav = document.querySelector(".list-nav-background");
+const listNav = document.querySelector(".nav-list");
+const backgroundNav = document.querySelector(".nav-background");
 
 iconMenu.addEventListener("click", () => {
-    console.log("Hello");
     backgroundNav.classList.toggle("show-nav");
     listNav.classList.toggle("show-nav");
     iconMenu.classList.toggle("icon-close");
 
     if (listNav.classList.contains("show-nav")) {
+        console.log("Nav open");
         disableBodyScroll(listNav);
     } else {
+        console.log("Nav closed");
         enableBodyScroll(listNav);
     }
 });
