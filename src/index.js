b689dd0548602d599485956fee02a72402c9ef0e
Cyrille Guillaume <cyrilleguillaume1996@gmail.com>
2020-10-17 00:53:23 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
 import "./assets/css/main.scss";
 import "./assets/js/app.js";
